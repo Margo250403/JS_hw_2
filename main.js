3952d7f4f21b19540cb9149ff9a64f0c28150db7
Margo250403 <margoshakac@gmail.com>
2022-02-06 15:28:56 +0200

    add code for hw 2

---
+++ main.js
@@ -0,0 +1,17 @@
+var names = prompt("Enter names:");
+
+var arr = names.split(', ');
+
+for (var arr_name of arr)
+{
+     console.log("<li>"+ arr_name + "</li>"); // Alex, Vasa, Pitya, Ann, Oleg
+}
+
+
+
+//  var names = prompt("Enter names:");
+//  var arr = names.split(",");
+//  for (var index of names)
+//  {
+//     console.log( + index + 1 + " " + arr_names);
+//  }
