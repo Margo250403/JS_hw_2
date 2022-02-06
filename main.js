d50ccbd2407715fb37e80c6298dc384392421b5e
Margo250403 <margoshakac@gmail.com>
2022-02-06 17:04:14 +0200

    add changes

--- main.js
+++ main.js
@@ -2,16 +2,7 @@ var names = prompt("Enter names:");
 
 var arr = names.split(', ');
 
-for (var arr_name of arr)
+for (var index in arr)
 {
-     console.log("<li>"+ arr_name + "</li>"); // Alex, Vasa, Pitya, Ann, Oleg
+     console.log("<li>" + (+index+1) + " " + arr[index] + "</li>"); // Alex, Vasa, Pitya, Ann, Oleg
 }
-
-
-
-//  var names = prompt("Enter names:");
-//  var arr = names.split(",");
-//  for (var index of names)
-//  {
-//     console.log( + index + 1 + " " + arr_names);
-//  }
